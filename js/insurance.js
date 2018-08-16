b8c0a7bf3bff7ca9210f3aac8545afb1f8cb4a67
SimpleLin <171250015@smail.nju.edu.cn>
2018-08-16 22:40:08 +0800

    组件装好

--- js/insurance.js
+++ js/insurance.js
@@ -1,6 +1,17 @@
 // JavaScript Document
+
+
 function getCreditScore()
 {
 	var score=280;
 	return score;
 }
+
+function figurePrice(){
+	var price=0;
+	price = document.getElementById("hour").value*0.16;
+	document.getElementById("price").innerHTML=price;
+}
+function jumpto(){
+			window.location.href='ofo2.html';
+		}
